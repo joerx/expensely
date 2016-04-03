52f51bb4d0984a5e415e2eb8712c0a969afa82cd
Joerg Henning <henning.joerg@gmail.com>
2016-04-03 15:02:16 +0800

    Static layout

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,121 @@
 import React from 'react';
 
-export default class App extends React.Component {
 
+export default class App extends React.Component {
   constructor(props) {
     super(props)
   }
-
   render() {
-    return (<h1>{this.props.title}</h1>)
+    return (
+      <div>
+        <h1 className="title">{this.props.title}</h1>
+        <InputField />
+        <ExpenseList data={this.props.data} />
+      </div>
+    )
+  }
+}
+
+class InputField extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <section className="row">
+        <div className="col col-md-12">
+          <form className="form">
+            <div className="form-group">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="e.g. Milk 2$"/>
+                <span className="input-group-btn">
+                  <button type="button" className="btn btn-primary">Enter</button>
+                </span>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    )
+  }
+}
+
+class ExpenseList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <section className="expense-list">
+        <SummaryRow />
+        <ExpenseHistory data={this.props.data} />
+      </section>
+    )
+  }
+}
+
+class SummaryRow extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="row summary-row">
+        <div className="col col-md-8">
+          <span className="summary-label">
+            Total so far:
+          </span>
+        </div>
+        <div className="col col-md-4 summary-amount">
+          <span>
+            123 S$
+          </span>
+          <a className="row-action">
+            <span className="glyphicon glyphicon-refresh"/>
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
+
+class ExpenseHistory extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const list = this.props.data.map((e, idx) => (
+      <HistoryRow id="idx" amount={e.amount}>{e.item}</HistoryRow>
+    ))
+    return (
+      <section className="expense-history">
+        {list}
+      </section>
+    )
+  }
+}
+
+class HistoryRow extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="row history-row">
+        <div className="col col-md-8">
+          <span className="row-label">
+            {this.props.children}
+          </span>
+        </div>
+        <div className="col col-md-4 row-amount">
+          <span>
+            {this.props.amount} S$
+          </span>
+          <a className="row-action">
+            <span className="glyphicon glyphicon-remove"/>
+          </a>
+        </div>
+      </div>
+    )
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,10 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
 
-render(<App title="Hello App!"/>, document.getElementById('app'));
+const data = [
+  {item: 'Milk', amount: 1.9},
+  {item: 'Bananas', amount: 1.2},
+  {item: 'Kitkat Share Pack', amount: 5.59}
+];
+
+render(<App title="My Foo" data={data} />, document.getElementById('app'));
