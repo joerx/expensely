20b95854f34aa34e3acb871f71e4c8da6df83e0b
Joerg Henning <henning.joerg@gmail.com>
2016-04-08 16:18:52 +0800

    Split up containers and display components

--- public/src/js/App.js
+++ public/src/js/App.js
@@ -13,21 +13,57 @@ function ucfirst(str) {
   return str[0].toUpperCase() + str.substr(1, str.length);
 }
 
-export default class App extends React.Component {
+// Containers
+// manages listing & removing expenses 
+class ExpenseHistoryContainer extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = props.store.getState();
+    props.store.subscribe(_ => 
+      this.setState(props.store.getState())
+    );
   }
+  render() {
+    return (<ExpenseList 
+      expenses={this.state.expenses} 
+      total={this.state.total}
+      onItemDelete={id => this.dispatchItemDelete(id)} 
+      onHistoryReset={_ => this.dispatchHistoryReset() }/>
+    );
+  }
+  dispatchItemDelete(id) {
+    this.props.store.dispatch(removeExpense(id));
+  }
+  dispatchHistoryReset() {
+    this.props.store.dispatch(resetExpenses());
+  }
+}
+
+// manages creating new expenses
+class AddExpense extends React.Component {
+  render() {
+    return <InputField onSubmit={item => this.dispatchAddExpense(item)} />
+  }
+  dispatchAddExpense({item, amount}) {
+    this.props.store.dispatch(addExpense(item, amount));
+  }
+}
+
+// App, presentational, renders everything else
+export default class App extends React.Component {
   render() {
     return (
       <div>
         <h1 className="title">{this.props.title}</h1>
-        <InputField store={this.props.store} />
-        <ExpenseList store={this.props.store} />
+        <AddExpense store={this.props.store} />
+        <ExpenseHistoryContainer store={this.props.store} />
       </div>
     )
   }
 }
 
+// Presentational component 
+// - onSubmit: fn({item, amount})
 export class InputField extends React.Component {
   constructor(props) {
     super(props);
@@ -38,15 +74,37 @@ export class InputField extends React.Component {
       'form-group', 
       !this.state.valid ? 'has-error' : undefined
     ].join(' ');
+
+    const validate = (e) => {
+      e.preventDefault();
+      const [item, amount, err] = parseInput(this.refs.input.value);
+      if (err) {
+        this.setState({valid: false});
+      } else {
+        this.setState({valid: true});
+        this.props.onSubmit({item, amount});
+        this.refs.input.value = '';
+        this.refs.input.focus();
+      }
+    }
+
     return (
       <section className="row">
         <div className="col col-md-12">
-          <form className="form" onSubmit={e => this.formSubmit(e)}>
+          <form className="form" onSubmit={validate}>
             <div className={formGroupClass}>
               <div className="input-group">
-                <input type="text" className="form-control" ref="input" placeholder="e.g. Milk 1.95"/>
+                <input
+                  type="text" 
+                  className="form-control" 
+                  ref="input" 
+                  placeholder="e.g. Milk 1.95"/>
                 <span className="input-group-btn">
-                  <button type="submit" className="btn btn-primary">Enter</button>
+                  <button 
+                    type="submit" 
+                    className="btn btn-primary">
+                    Enter
+                  </button>
                 </span>
               </div>
             </div>
@@ -55,44 +113,32 @@ export class InputField extends React.Component {
       </section>
     );
   }
-  formSubmit(e) {
-    e.preventDefault();
-    const [item, amount, err] = parseInput(this.refs.input.value);
-    if (err) {
-      this.setState({valid: false});
-    } else {
-      this.setState({valid: true});
-      this.props.store.dispatch(addExpense(item, amount));
-      this.refs.input.value = '';
-      this.refs.input.focus();
-    }
-  }
 }
 
+// List of expenses - display and manipulation of history
+// - total: number
+// - expenses: array[expense]
+// - onHistoryItemDelete: fn()
+// - onHistoryReset: fn()
 export class ExpenseList extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <section className="expense-list">
-        <SummaryRow store={this.props.store} />
-        <ExpenseHistory store={this.props.store} />
+        <SummaryRow 
+          amount={this.props.total}
+          onReset={_ => this.props.onHistoryReset()} />
+        <ExpenseHistory 
+          expenses={this.props.expenses}
+          onItemDelete={id => this.props.onItemDelete(id)} />
       </section>
     )
   }
 }
 
+// Summary row, presentational 
+// - amount: number
+// - onResetClicked: fn()
 export class SummaryRow extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      amount: props.store.getState().total
-    }
-    props.store.subscribe(_ => 
-      this.setState({amount: props.store.getState().total})
-    )
-  }
   render() {
     return (
       <div className="row summary-row">
@@ -103,7 +149,7 @@ export class SummaryRow extends React.Component {
         </div>
         <div className="col col-md-4 summary-amount">
           <span>
-            {printf('%.2f S$', this.state.amount)}
+            {printf('%.2f S$', this.props.amount)}
           </span>
           <a className="row-action" onClick={_ => this.resetClicked()}>
             <span className="glyphicon glyphicon-refresh"/>
@@ -113,30 +159,20 @@ export class SummaryRow extends React.Component {
     )
   }
   resetClicked() {
-    if (confirm('Are you sure?') === true) {
-      this.props.store.dispatch(resetExpenses())
-    }
+    confirm('Are you sure?') && this.props.onReset()
   }
 }
 
+// List of expenses
+// - expenses: array[expense]
+// - onHistoryItemDelete: fn(id)
 export class ExpenseHistory extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {expenses: props.store.getState().expenses};
-    props.store.subscribe(_ => this.storeHasChanged())
-  }
-  storeHasChanged() {
-    this.setState({expenses: this.props.store.getState().expenses});
-  }
   render() {
-    const list = this.state.expenses.map((e, idx) => {
-      const id = this.state.expenses.indexOf(e);
+    const list = this.props.expenses.map((e, idx) => {
+      const id = this.props.expenses.indexOf(e);
       return (
-        <HistoryRow 
-          key={id}
-          id={id}
-          amount={e.amount}
-          store={this.props.store}>
+        <HistoryRow key={id} amount={e.amount}
+          onDelete={_ => this.props.onItemDelete(id)}>
           {e.item}
         </HistoryRow>)
       })
@@ -148,10 +184,9 @@ export class ExpenseHistory extends React.Component {
   }
 }
 
+// Single expense
+// - onDelete()
 export class HistoryRow extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <div className="row history-row">
@@ -164,14 +199,11 @@ export class HistoryRow extends React.Component {
           <span>
             {printf('%.2f S$', this.props.amount)}
           </span>
-          <a className="row-action" onClick={_ => this.removeRowClicked()}>
+          <a className="row-action" onClick={_ => this.props.onDelete()}>
             <span className="glyphicon glyphicon-remove"/>
           </a>
         </div>
       </div>
     )
-  }
-  removeRowClicked() {
-    this.props.store.dispatch(removeExpense(this.props.id));
   }
 }
