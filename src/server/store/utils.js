6937c07b9caf23eb5df95e11514c878ee17422ef
Jörg Henning <henning.joerg@gmail.com>
2017-05-14 13:05:51 +0530

    Change wording in store error message

--- src/server/store/utils.js
+++ src/server/store/utils.js
@@ -4,6 +4,6 @@ const error = require('http-errors');
 
 exports.translateDbError = (err) => {
   return Promise.reject(
-    error.InternalServerError('database '+err.message)
+    error.InternalServerError('Store error: \''+err.message+'\'')
   );
 }
